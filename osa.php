6cabc224d0f0c11e51bb619b014c5abcaaf4b4a0
William Suarez <wsuarez86@hotmail.com>
2016-11-21 08:43:04 -0500

    archivo ejemplo

---
+++ osa.php
@@ -0,0 +1,19 @@
+
+<h2>Hola </h2>
+
+<form>
+<label>Nombre:</label>
+<input type="text" value="Osa"></input>
+
+<button>Enviar datos</button>
+EJEMPLO
+
+</form>
+
+
+<?php 
+
+echo "Hola mundo";
+
+
+?>
